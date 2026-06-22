56df5d533dfef3022582ce37b4d13c2e47fdcf23
Anjali-Gangwar <anjaligangwar091@gmail.com>
2026-06-22 23:02:05 +0530

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.java
@@ -0,0 +1,30 @@
+
+
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+            int n = heights.length;
+                    Stack<Integer> st = new Stack<>();
+                            int maxArea = 0;
+
+                                    for (int i = 0; i <= n; i++) {
+                                                int currHeight = (i == n) ? 0 : heights[i];
+
+                                                            while (!st.isEmpty() && currHeight < heights[st.peek()]) {
+                                                                            int height = heights[st.pop()];
+
+                                                                                            int width;
+                                                                                                            if (st.isEmpty()) {
+                                                                                                                                width = i;
+                                                                                                                                                } else {
+                                                                                                                                                                    width = i - st.peek() - 1;
+                                                                                                                                                                                    }
+
+                                                                                                                                                                                                    maxArea = Math.max(maxArea, height * width);
+                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                            st.push(i);
+                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                            return maxArea;
+                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                }
